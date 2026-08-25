9611a74f1c92984205af63404400ef38de236c56
Venus Poon <venuspsw@hotmail.com>
2026-08-26 00:02:43 +0200

    initial commit

---
+++ BookQuoteApi/Data/AppDatabase.cs
@@ -0,0 +1,15 @@
+﻿using BookQuoteApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookQuoteApi.Data
+{
+    public class AppDatabase
+    {
+        public AppDatabase(DbContextOptions<AppDatabase> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Book> Books => Set<Book>();
+    }
+}
---
+++ BookQuoteApi/Models/Quote.cs
@@ -0,0 +1,6 @@
+﻿namespace BookQuoteApi.Models
+{
+    public class Quote
+    {
+    }
+}
