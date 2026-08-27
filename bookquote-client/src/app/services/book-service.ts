89d71aa2460e69e91b4a9a30234860cdadb26b99
Venus Poon <venuspsw@hotmail.com>
2026-08-28 01:04:56 +0200

    add enviro url

--- bookquote-client/src/app/services/book-service.ts
+++ bookquote-client/src/app/services/book-service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 import { CreateBook } from '../models/create-book';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private apiUrl = 'http://localhost:5138/api/books';
+  private apiUrl = environment.apiUrl;
+
 
   constructor(private http: HttpClient) {}
 
