c16f5c1d199ccabc25f228de1009ec5759f1b72f
Venus Poon <venuspsw@hotmail.com>
2026-08-28 00:38:49 +0200

    seed initial books

--- BookQuoteApi/Data/AppDbContext.cs
+++ BookQuoteApi/Data/AppDbContext.cs
@@ -1,15 +1,43 @@
 ﻿using BookQuoteApi.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookQuoteApi.Data
-{
-    public class AppDbContext:DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-        {
-        }
+namespace BookQuoteApi.Data;
 
-        public DbSet<Book> Books => Set<Book>();
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Book>().HasData(
+            new Book
+            {
+                Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                Title = "The Hobbit",
+                Author = "J.R.R. Tolkien",
+                PublicationDate = new DateTime(1937, 9, 21)
+            },
+            new Book
+            {
+                Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                Title = "1984",
+                Author = "George Orwell",
+                PublicationDate = new DateTime(1949, 6, 8)
+            },
+            new Book
+            {
+                Id = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                Title = "Pride and Prejudice",
+                Author = "Jane Austen",
+                PublicationDate = new DateTime(1813, 1, 28)
+            }
+        );
     }
 }
---
+++ BookQuoteApi/Migrations/20260827223719_SeedBooks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookQuoteApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "PublicationDate", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "J.R.R. Tolkien", new DateTime(1937, 9, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Hobbit" },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "George Orwell", new DateTime(1949, 6, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), "1984" },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), "Jane Austen", new DateTime(1813, 1, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), "Pride and Prejudice" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+        }
+    }
+}
