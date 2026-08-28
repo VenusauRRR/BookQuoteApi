2d3f880ea9d93d55679cd2eae3aa40ced048ae98
Venus Poon <venuspsw@hotmail.com>
2026-08-28 02:08:15 +0200

    rename vercel url to cros

--- BookQuoteApi/Program.cs
+++ BookQuoteApi/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "https://YOUR-VERCEL-APP.vercel.app")
+        policy.WithOrigins("http://localhost:4200", "https://book-quote-client.vercel.app/")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
