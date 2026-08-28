7db00bece2e8b9b60d3b51ee505235abea17c5cb
Venus Poon <venuspsw@hotmail.com>
2026-08-29 01:01:46 +0200

    add clockskew to timespan.zero to get token expired

--- BookQuoteApi/Program.cs
+++ BookQuoteApi/Program.cs
@@ -41,7 +41,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(
-                    builder.Configuration["Jwt:Key"]!))
+                    builder.Configuration["Jwt:Key"]!)),
+            ClockSkew = TimeSpan.Zero
         };
     });
 
