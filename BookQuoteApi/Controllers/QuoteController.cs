0103b87e165c5c69c1b7ffff15ca7f35176a9d10
Venus Poon <venuspsw@hotmail.com>
2026-08-29 16:51:53 +0200

    add getQuotes from user

--- BookQuoteApi/Controllers/QuoteController.cs
+++ BookQuoteApi/Controllers/QuoteController.cs
@@ -1,15 +1,29 @@
 ﻿using BookQuoteApi.Data;
+using BookQuoteApi.DTOs;
 using BookQuoteApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace BookQuoteApi.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/quotes")]
 public class QuoteController(AppDbContext db) : ControllerBase
 {
     private readonly AppDbContext _context = db;
+
+    private Guid? GetCurrentUserId()
+    {
+        var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userIdString == null)
+        {
+            return null;
+        }
+        return Guid.Parse(userIdString);
+    }
 
     [HttpGet("test")]
     public IActionResult Test()
@@ -18,23 +32,62 @@ public class QuoteController(AppDbContext db) : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Quote>>> GetQuotes()
+    public async Task<ActionResult<IEnumerable<Quote>>> GetAllQuotes()
     {
         return await _context.Quotes.ToListAsync();
     }
 
-
-    [HttpGet("get/{userId}")]
-    public async Task<ActionResult<IEnumerable<Quote>>> GetQuotesByuserId(string userId)
+    [HttpGet("get/{id}")]
+    public async Task<ActionResult<Quote>> GetQuoteById(Guid id)
     {
-        var quotes = await _context.Quotes.Where(q => q.UserId.ToString() == userId).ToListAsync();
+        var userId = GetCurrentUserId();
 
-        if (quotes == null)
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+        var quote = await _context.Quotes.FirstOrDefaultAsync(q => q.Id==id && q.UserId == userId);
+
+        if (quote == null)
         {
             return NotFound();
         }
 
+        return quote;
+    }
+
+    [HttpGet("get-my-quotes")]
+    public async Task<ActionResult<IEnumerable<Quote>>> GetMyQuotes()
+    {
+        var userId = GetCurrentUserId();
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+        var quotes = await _context.Quotes.Where(q => q.UserId == userId).ToListAsync();
+
         return quotes;
+    }
+
+
+    [HttpPost("add")]
+    public async Task<ActionResult<Quote>> CreateQuote(QuoteRequest request)
+    {
+        var userId = GetCurrentUserId();
+        var newQuote = new Quote
+        {
+            QuoteText = request.QuoteText,
+            UserId = userId.Value,
+        };
+
+
+        _context.Quotes.Add(newQuote);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetQuoteById),
+            new { id = newQuote.Id },
+            newQuote);
     }
 
 }
