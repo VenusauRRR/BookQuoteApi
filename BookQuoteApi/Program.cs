058a0bf6ece61ab62e8954beea13685a7a29ef6c
Venus Poon <venuspsw@hotmail.com>
2026-08-29 14:06:46 +0200

    put back migration

--- BookQuoteApi/Program.cs
+++ BookQuoteApi/Program.cs
@@ -53,13 +53,13 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Apply migrations
-//using (var scope = app.Services.CreateScope())
-//{
-//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//    db.Database.Migrate();
-//}
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
-// Configure the HTTP request pipeline.
+Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
